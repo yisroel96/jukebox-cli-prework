5807ffca6dc072091977d6fd3c5be4f0bf2c2fbb
witty-collection-3981 <yisroel96@gmail.com>
2018-11-30 09:04:08 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -10,3 +10,27 @@ songs = [
   "Amos Lee - Keep It Loose, Keep It Tight"
 ]
 
+def help
+  puts "I accept the following commands:
+- help : displays this help message
+- list : displays a list of songs you can play
+- play : lets you choose a song to play
+- exit : exits this program"
+end
+
+def list(songs)
+  puts "1. Phoenix - 1901
+2. Tokyo Police Club - Wait Up
+3. Sufjan Stevens - Too Much
+4. The Naked and the Famous - Young Blood
+5. (Far From) Home - Tiga
+6. The Cults - Abducted
+7. Phoenix - Consolation Prizes
+8. Harry Chapin - Cats in the Cradle
+9. Amos Lee - Keep It Loose, Keep It Tight"
+end
+
+def play(songs)
+  puts "Please enter a song name or number:"
+  gets.chomp
+end
